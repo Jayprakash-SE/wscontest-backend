1edb03e2b7b54931d889e3a1cace978cf950321b
Guggilam Sai Prabhat <84320979+Guggilam-sai-prabhat@users.noreply.github.com>
2023-02-26 11:20:35 +0530

    API to remove index page (#4)

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
+# ------- Index route -------
+@app.route('/')
+def index():
+    return redirect(app.config["APP_REDIRECT_URI"])
+
+
 # ------- Create Contest -------
 @app.route('/api/contest/create', methods=['POST'])
 def createContest():
@@ -144,12 +151,8 @@ def createContest():
     return jsonify({
         "status" : True,
         "message" : "Contest Created"
-    }),200 
+    }),200
 
-# ------- Index route -------
-@app.route('/')
-def index():
-    return redirect(app.config["APP_REDIRECT_URI"])
 
 @app.route('/api/index/add',methods=["POST"])
 def indexpages():
@@ -223,6 +226,30 @@ def indexpages():
         "staus": True,
         "message": "Sucesfully added"
     }),200
+
+
+@app.route('/api/index/remove', methods=["POST"])
+def indexpage_remove():
+    data = request.json 
+
+    if not "idpd" in data.keys():
+        return jsonify({
+            "status": False,
+            "message": "It doesn't exist"
+        }), 400
+    
+    if "idpd" == "":
+        return jsonify({
+            "status": False,
+            "message": "It is empty"
+        }), 400
+    
+    IndexPages.query.filter_by(idpb=data["idpb"]).delete()
+
+    return jsonify({
+        "status": True,
+        "message": "Sucessfuly removed"
+    }), 200
 
 
 if __name__ == "__main__":
