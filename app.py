91a3fb859afb7b60beeab4235ffe6b1f3cf1f297
Jayprakash <0freerunning@gmail.com>
2023-02-26 11:26:57 +0530

    Update app.py
    Rearrange the routes

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, redirect, request, jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from models import Contests, IndexPages
-
-from models import ContestAdmin
+from models import Contests, IndexPages, ContestAdmin
 
 app = Flask(__name__)
 
@@ -18,44 +16,6 @@ migrate = Migrate(app, db)
 @app.route('/')
 def index():
     return redirect(app.config["APP_REDIRECT_URI"])
-
-@app.route('/api/admin/add',methods=['POST'])
-def add_admin():
-    data = request.json
-
-    if not "user_name"in data.keys():
-        return jsonify({
-            "status": False,
-            "message":"Username is not there"
-        }), 400
-    
-    if data["user_name"] == "":
-        return jsonify({
-            "status": False,
-            "message":"Username is empty"
-        }), 400
-
-
-    if not "contest_id" in data.keys():
-        return jsonify({
-            "status": False,
-            "message":"customer_id is not there"
-        }), 400
-    
-    if data["contest_id"] == "":
-        return jsonify({
-            "status": False,
-            "message":"customer_id is empty"
-        }), 400
-
-    cadmin= ContestAdmin(c_id= data["contest_id"], user_name= data["user_name"])
-    db.session.add(cadmin)
-    db.session.commit()
-
-    return jsonify({
-        "status": True,
-        "message":"Admin is added"
-    }), 200
 
 # ------- Create Contest -------
 @app.route('/api/contest/create', methods=['POST'])
@@ -288,6 +248,45 @@ def indexpage_remove():
     return jsonify({
         "status": True,
         "message": "Sucessfuly removed"
+    }), 200
+
+
+@app.route('/api/admin/add',methods=['POST'])
+def add_admin():
+    data = request.json
+
+    if not "user_name"in data.keys():
+        return jsonify({
+            "status": False,
+            "message":"Username is not there"
+        }), 400
+    
+    if data["user_name"] == "":
+        return jsonify({
+            "status": False,
+            "message":"Username is empty"
+        }), 400
+
+
+    if not "contest_id" in data.keys():
+        return jsonify({
+            "status": False,
+            "message":"customer_id is not there"
+        }), 400
+    
+    if data["contest_id"] == "":
+        return jsonify({
+            "status": False,
+            "message":"customer_id is empty"
+        }), 400
+
+    cadmin= ContestAdmin(c_id= data["contest_id"], user_name= data["user_name"])
+    db.session.add(cadmin)
+    db.session.commit()
+
+    return jsonify({
+        "status": True,
+        "message":"Admin is added"
     }), 200
 
 
