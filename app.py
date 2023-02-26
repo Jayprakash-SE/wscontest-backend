ab885347f2086cc6fb7db8d3e22224aeb58da11f
Ankith-Suravarapu <83510934+Ankith-Suravarapu@users.noreply.github.com>
2023-02-26 11:53:46 +0530

    Create "/api/admin/remove" endpoint

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ migrate = Migrate(app, db)
 def index():
     return redirect(app.config["APP_REDIRECT_URI"])
 
+
 # ------- Create Contest -------
 @app.route('/api/contest/create', methods=['POST'])
 def createContest():
@@ -251,7 +252,7 @@ def indexpage_remove():
     }), 200
 
 
-@app.route('/api/admin/add',methods=['POST'])
+@app.route('/api/admin/add', methods=['POST'])
 def add_admin():
     data = request.json
 
@@ -289,6 +290,41 @@ def add_admin():
         "message":"Admin is added"
     }), 200
 
+@app.route('/api/admin/remove', methods=['POST'])
+def remove_admin():
+    data = request.json
+
+    if not "user_name"in data.keys():
+        return jsonify({
+            "status": False,
+            "message":"Username is not there"
+        }), 400
+    
+    if data["user_name"] == "":
+        return jsonify({
+            "status": False,
+            "message":"Username is empty"
+        }), 400
+
+
+    if not "contest_id" in data.keys():
+        return jsonify({
+            "status": False,
+            "message":"customer_id is not there"
+        }), 400
+    
+    if data["contest_id"] == "":
+        return jsonify({
+            "status": False,
+            "message":"customer_id is empty"
+        }), 400
+
+    ContestAdmin.query.filter_by(c_id=data["contest_id"], user_name=data["user_name"]).delete()
+
+    return jsonify({
+        "status": True,
+        "message":"Admin is removed"
+    }), 200
 
 if __name__ == "__main__":
     app.run(debug  = True)
