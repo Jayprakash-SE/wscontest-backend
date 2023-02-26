e8b62adbbcdd43ac6884c20b7befeee4859a50a5
Prabhat0321 <guggliamprabhat@gmail.com>
2023-02-26 11:15:27 +0530

    API to remove index page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect
+from flask import Flask, redirect , request , jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from models import IndexPages
 
 app = Flask(__name__)
 
@@ -16,6 +17,28 @@ migrate = Migrate(app, db)
 def index():
     return redirect(app.config["APP_REDIRECT_URI"])
 
+@app.route('/api/index/remove', methods=["POST"])
+def indexpage_remove():
+    data = request.json 
+
+    if not "idpd" in data.keys():
+        return jsonify({
+            "status": False,
+            "message": "It doesn't exist"
+        }), 400
+    
+    if "idpd" == "":
+        return jsonify({
+            "status": False,
+            "message": "It is empty"
+        }), 400
+    
+    IndexPages.query.filter_by(idpb=data["idpb"]).delete()
+
+    return jsonify({
+        "status": True,
+        "message": "Sucessfuly removed"
+    }), 200
 
 if __name__ == "__main__":
     app.run()
